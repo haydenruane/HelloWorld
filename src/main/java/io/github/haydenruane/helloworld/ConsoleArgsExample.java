3d3b60504b10353265392e41ae3cbc5812eb1d47
Hayden Ruane <94908148+haydenruane@users.noreply.github.com>
2021-11-23 22:40:22 +0000

    My First Commit

---
+++ src/main/java/io/github/haydenruane/helloworld/ConsoleArgsExample.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2021 Hayden Ruane <https://github.com/haydenruane>. All rights reserved.
+ */
+package io.github.haydenruane.helloworld;
+
+import java.util.Arrays;
+
+public class ConsoleArgsExample
+{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		System.out.print(Arrays.toString(args));
+	}
+}
---
+++ src/main/java/io/github/haydenruane/helloworld/HelloWorldExample.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 Hayden Ruane <https://github.com/haydenruane>. All rights reserved.
+ */
+package io.github.haydenruane.helloworld;
+
+public class HelloWorldExample
+{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Mark, thank you for coming");
+	}
+
+}
