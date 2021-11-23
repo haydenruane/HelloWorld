09b10be4feb3d088b9255a1f998b6e6b9e3d241f
Hayden Ruane <94908148+haydenruane@users.noreply.github.com>
2021-11-23 22:51:57 +0000

    Create VariableExample.java

---
+++ src/main/java/io/github/haydenruane/helloworld/VariableExample.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021 Hayden Ruane <https://github.com/haydenruane>. All rights reserved.
+ */
+package io.github.haydenruane.helloworld;
+
+public class VariableExample
+{
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		int number1 = 1;
+		int number2 = 2;
+		System.out.println(number1);
+		System.out.println(number2);
+	}
+}
